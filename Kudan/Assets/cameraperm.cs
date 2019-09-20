c863a00ca860ab2dda5f78865815bbc20278d5c4
Colin Cop <colincop@ucm.es>
2019-09-20 10:37:03 +0200

    añadidas las librerias restantes

---
+++ Kudan/Assets/cameraperm.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if PLATFORM_ANDROID
+using UnityEngine.Android;
+#endif
+
+public class cameraperm : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        Permission.RequestUserPermission(Permission.Camera);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
